358f1cdeb00729aa3a992f61dc128f4cad201b2c
iqbalaulia <miqbalar12@gmail.com>
2021-10-13 13:56:37 +0700

    integrasi api radius distrik

--- src/services/connection/radius-distrik/index.js
+++ src/services/connection/radius-distrik/index.js
@@ -28,10 +28,33 @@ const state = {
         slots: { customRender: 'action' },
       },
     ],
+    dataDistrik: [],
     isLoading: false,
     bodyList: {
       offset: 1,
       limit: 20,
+    },
+    rules: {
+      distrikid: [
+        {
+          required: true,
+          message: 'Pilih Salah Satu Distrik',
+          type: 'number',
+        },
+      ],
+      radius: [
+        {
+          required: true,
+          message: 'Masukkan Nilai Radius',
+          type: 'number',
+        },
+      ],
+    },
+    formData: {
+      id: null,
+      rownum: null,
+      distrikid: undefined,
+      radius: 0,
     },
   },
 }
@@ -64,7 +87,61 @@ const actions = {
       })
     } else {
       await commit('changeRadiusDistrik', {
-        users: result.data.data,
+        listRadiusDistrik: result.data.data,
+        isLoading: false,
+      })
+    }
+  },
+
+  async getDataDistrik({ commit, state }) {
+    commit('changeRadiusDistrik', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    let body = {
+      offset: data.bodyList.offset,
+      limit: data.bodyList.limit,
+    }
+
+    const result = await apiClient.post('/RadiusDistrik/List_Distrik', body)
+
+    if (result.data.status == 'error') {
+      notification.error({
+        message: 'Error',
+        description: result.data.message,
+      })
+    } else {
+      await commit('changeRadiusDistrik', {
+        dataDistrik: result.data.data,
+        isLoading: false,
+      })
+    }
+  },
+
+  async postDataRadiusDistrik({ commit, state }) {
+    commit('changeRadiusDistrik', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    let body = {
+      IDdistrik: data.formData.distrikid,
+      radius_lock: data.formData.radius,
+    }
+
+    const result = await apiClient.post('/RadiusDistrik/Set_Radius', body)
+
+    if (result.data.status == 'error') {
+      notification.error({
+        message: 'Error',
+        description: result.data.message,
+      })
+    } else {
+      await commit('changeRadiusDistrik', {
+        dataDistrik: result.data.data,
         isLoading: false,
       })
     }
