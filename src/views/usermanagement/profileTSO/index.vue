31ae766254a8700a44d66c8f323a4421b04cdf6c
iqbalaulia <miqbalar12@gmail.com>
2021-10-27 10:21:38 +0700

    fixing detail TSO

--- src/views/usermanagement/profileTSO/index.vue
+++ src/views/usermanagement/profileTSO/index.vue
@@ -8,91 +8,129 @@
         >
       </div>
     </div>
-    <div class="card card-top card-top-primary">
-      <div class="card-header">
-        <strong>Profile TSO</strong>
-      </div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-xs-3 col-md-3">
-            <a-form-item label="List Distrik">
-              <a-select
-                v-model:value="profileTSO.formData.id_distrik"
-                style="width: 10rem !important;"
-                class="col-lg-12 col-md-12 pr-2"
-                placeholder="Pilih Distrik"
-              >
-                <a-select-option disabled value="">Pilih Salah Satu</a-select-option>
-                <a-select-option
-                  v-for="(item, index) in profileTSO.daftar_distrik"
-                  :value="item.idDistrik"
-                  :key="`distrik_${index}`"
-                >
-                  {{ item.namaDistrik }}
-                </a-select-option>
-              </a-select>
-            </a-form-item>
+    <div class="row">
+      <div class="col-md-4 col-xs-4">
+        <div class="card card-top card-top-primary">
+          <div class="card-header d-flex">
+            <strong class="align-self-center">Mapping User</strong>
           </div>
-          <div class="col-xs-3 col-md-3">
-            <a-form-item label="Tgl Mulai">
-              <a-date-picker
-                v-model:value="profileTSO.formData.tgl_mulai"
-                placeholder="Pilih Tanggal"
-                style="width: 10rem !important;"
-              />
-            </a-form-item>
+          <div class="card-header">
+            <div class="d-flex flex-wrap flex-column align-items-center">
+              <div class="vb__utils__avatar vb__utils__avatar--size64 mb-3">
+                <img
+                  lazy="loading"
+                  v-once
+                  :src="require('@/assets/images/users.png')"
+                  alt="Mary Stanform"
+                />
+              </div>
+              <div class="text-center">
+                <div class="text-dark font-weight-bold font-size-20">
+                  {{ userManagement.detail_jabatan.namaUser }}
+                </div>
+                <div class="font-size-16">
+                  Kode / ID : {{ userManagement.detail_jabatan.idUser }}
+                </div>
+                <div class="font-size-16">
+                  Username : {{ userManagement.detail_jabatan.namaUser }}
+                </div>
+                <div class="font-size-16">Email : {{ userManagement.detail_jabatan.email }}</div>
+              </div>
+            </div>
           </div>
-          <div class="col-xs-3 col-md-3">
-            <a-form-item label="Tgl Akhir">
-              <a-date-picker
-                v-model:value="profileTSO.formData.tgl_akhir"
-                placeholder="Pilih Tanggal"
-                style="width: 10rem !important;"
-              />
-            </a-form-item>
-          </div>
-          <div class="col-xs-3 col-md-3">
-            <a-button type="primary" class="mb-3 w-100" @click="handleSubmit()">
-              <i class="fa fa-eye mr-2" />
-              Tambahkan
-            </a-button>
+          <div class="card-header align-self-center">
+            <strong>Jenis User : {{ userManagement.detail_jabatan.namaJabatan }}</strong>
           </div>
         </div>
+      </div>
+      <div class="col-md-8 col-xs-8">
+        <div class="card card-top card-top-primary">
+          <div class="card-header">
+            <strong>Profile TSO</strong>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-xs-3 col-md-3">
+                <a-form-item label="">
+                  <a-select
+                    v-model:value="profileTSO.formData.id_distrik"
+                    style="width: 10rem !important;"
+                    class="col-lg-12 col-md-12 pr-2"
+                    placeholder="Pilih Distrik"
+                  >
+                    <a-select-option disabled value="">Pilih Salah Satu</a-select-option>
+                    <a-select-option
+                      v-for="(item, index) in profileTSO.daftar_distrik"
+                      :value="item.idDistrik"
+                      :key="`distrik_${index}`"
+                    >
+                      {{ item.namaDistrik }}
+                    </a-select-option>
+                  </a-select>
+                </a-form-item>
+              </div>
+              <div class="col-xs-3 col-md-3">
+                <a-form-item label="">
+                  <a-date-picker
+                    v-model:value="profileTSO.formData.tgl_mulai"
+                    placeholder="Tanggal Mulai"
+                    style="width: 10rem !important;"
+                  />
+                </a-form-item>
+              </div>
+              <div class="col-xs-3 col-md-3">
+                <a-form-item label="">
+                  <a-date-picker
+                    v-model:value="profileTSO.formData.tgl_akhir"
+                    placeholder="Tanggal Akhir"
+                    style="width: 10rem !important;"
+                  />
+                </a-form-item>
+              </div>
+              <div class="col-xs-3 col-md-3">
+                <a-button type="primary" class="" @click="handleSubmit()">
+                  <i class="fa fa-eye mr-2" />
+                  Tambahkan
+                </a-button>
+              </div>
+            </div>
 
-        <div class="table-responsive text-nowrap">
-          <a-table
-            :columns="profileTSO.columns"
-            :data-source="profileTSO.list_distrik_bawahan"
-            :row-key="data => data.idDistrik"
-            :loading="profileTSO.isLoading"
-          >
-            <template #no="{ index }">
-              <div>
-                {{ index + 1 }}
-              </div>
-            </template>
-            <template #id_distrik="{ text }">
-              <div>
-                {{ text.idDistrik != null ? text.idDistrik : '-' }}
-              </div>
-            </template>
-            <template #nama_distrik="{ text }">
-              <div>
-                {{ text.namaDistrik != null ? text.namaDistrik : '-' }}
-              </div>
-            </template>
-            <template #action="{ text }">
-              <div>
-                <button
-                  @click="deleteRow(text.idDistrik)"
-                  type="button"
-                  class="btn btn-outline-danger mr-2"
-                >
-                  <i class="fa fa-trash"></i><span> Hapus</span>
-                </button>
-              </div>
-            </template>
-          </a-table>
+            <div class="table-responsive text-nowrap">
+              <a-table
+                :columns="profileTSO.columns"
+                :data-source="profileTSO.list_distrik_bawahan"
+                :row-key="data => data.idDistrik"
+                :loading="profileTSO.isLoading"
+              >
+                <template #no="{ index }">
+                  <div>
+                    {{ index + 1 }}
+                  </div>
+                </template>
+                <template #id_distrik="{ text }">
+                  <div>
+                    {{ text.idDistrik != null ? text.idDistrik : '-' }}
+                  </div>
+                </template>
+                <template #nama_distrik="{ text }">
+                  <div>
+                    {{ text.namaDistrik != null ? text.namaDistrik : '-' }}
+                  </div>
+                </template>
+                <template #action="{ text }">
+                  <div>
+                    <button
+                      @click="deleteRow(text.idDistrik)"
+                      type="button"
+                      class="btn btn-outline-danger mr-2"
+                    >
+                      <i class="fa fa-trash"></i><span> Hapus</span>
+                    </button>
+                  </div>
+                </template>
+              </a-table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -121,11 +159,15 @@ export default {
   computed: {
     ...mapState({
       profileTSO: state => state.profileTSO.data,
+      userManagement: state => state.userManagement.data,
     }),
   },
   async mounted() {
     await this.getListDistrik({
       id_tso: this.$route.params.id,
+    })
+    await this.getDetailProfile({
+      id_jabatan: this.$route.params.id,
     })
     await this.getListDistrikHirarki({
       id_tso: this.$route.params.id,
@@ -138,7 +180,7 @@ export default {
       'deleteListDistrikHirarki',
       'addDistrikHirarki',
     ]),
-
+    ...mapActions('userManagement', ['getDetailProfile']),
     deleteRow(id_distriks) {
       this.$confirm({
         title: 'Apakah anda yakin akan menghapus data ini?',
@@ -172,6 +214,13 @@ export default {
       })
       await this.getListDistrikHirarki({
         id_tso: this.$route.params.id,
+      })
+      this.$store.commit('profileTSO/changeProfileTSO', {
+        formData: {
+          id_distrik: null,
+          tgl_mulai: '',
+          tgl_akhir: '',
+        },
       })
     },
   },
