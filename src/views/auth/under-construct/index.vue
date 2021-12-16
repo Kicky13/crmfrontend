42904b1fef528b8ae06250dbbf328b95de0edf0b
Rizki Akbar <kicky120@gmail.com>
2021-12-16 13:37:45 +0700

    Penambahan page error

---
+++ src/views/auth/under-construct/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <vb-under-construct />
+  </div>
+</template>
+<script>
+import VbUnderConstruct from '@/components/main/Errors/UnderConstruct'
+export default {
+  components: {
+    VbUnderConstruct,
+  },
+}
+</script>
