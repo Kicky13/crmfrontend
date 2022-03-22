88ea45582deaf6b3198fa1a061b0e2d3b235b6b7
iqbalaulia <iqbal.developer16@gmail.com>
2022-03-22 11:41:28 +0700

    [update] iframe baru

--- src/views/dashboard/promotion/index.vue
+++ src/views/dashboard/promotion/index.vue
@@ -7,6 +7,13 @@
       height="600"
       allowtransparency
     ></iframe>
+    <!-- <iframe
+      src="https://mtb.aksestoko.com/public/dashboard/47812e1e-1dc4-4c26-a2ee-e4b32541e8ab"
+      frameborder="0"
+      width="800"
+      class="w-100"
+      allowtransparency
+    ></iframe> -->
   </div>
 </template>
 
