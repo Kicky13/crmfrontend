ff2757af9c2421820ac05848e18d99b3b0df564f
iqbalaulia <iqbal.developer16@gmail.com>
2022-04-06 10:50:24 +0700

    [update] jadwal  magang

--- src/services/connection/weekly-price/weekly-input/index.js
+++ src/services/connection/weekly-price/weekly-input/index.js
@@ -6,67 +6,67 @@ const state = {
     columns: [
       {
         title: 'Distrik',
-        dataIndex: 'distrik',
+        slots: { customRender: 'distrik' },
         key: 'distrik',
       },
       {
         title: 'Tahun',
-        dataIndex: 'tahun',
+        slots: { customRender: 'tahun' },
         key: 'tahun',
       },
       {
         title: 'Bulan',
-        dataIndex: 'bulan',
+        slots: { customRender: 'bulan' },
         key: 'bulan',
       },
       {
         title: 'Week',
-        dataIndex: 'week',
+        slots: { customRender: 'week' },
         key: 'week',
       },
       {
         title: 'Status',
-        dataIndex: 'status',
+        slots: { customRender: 'status' },
         key: 'status',
       },
       {
         title: 'Produk',
-        dataIndex: 'produk',
+        slots: { customRender: 'produk' },
         key: 'produk',
       },
       {
         title: 'Brand',
-        dataIndex: 'brand',
+        slots: { customRender: 'brand' },
         key: 'brand',
       },
       {
         title: 'Type',
-        dataIndex: 'type',
+        slots: { customRender: 'type' },
         key: 'type',
       },
       {
         title: 'RBP Gross',
-        dataIndex: 'rbp_gross',
+        slots: { customRender: 'rbp_gross' },
         key: 'rbp_gross',
       },
       {
         title: 'Promo',
-        dataIndex: 'promo',
+        slots: { customRender: 'promo' },
         key: 'promo',
       },
       {
         title: 'RBP Net',
-        dataIndex: 'rbp_net',
+        slots: { customRender: 'rbp_net' },
         key: 'rbp_net',
       },
       {
         title: 'RSP',
-        dataIndex: 'rsp',
+        slots: { customRender: 'rsp' },
         key: 'rsp',
       },
       {
         title: 'Notes',
-        dataIndex: 'notes',
+        slots: { customRender: 'notes' },
         key: 'notes',
       },
       {
@@ -74,23 +74,93 @@ const state = {
         slots: { customRender: 'action' },
       },
     ],
-    weeklyInputList: [
+    data_bulan: [
       {
-        distrik: 'Distrik 1',
-        tahun: '2022',
-        bulan: 'Maret',
-        week: 'Week 1',
-        status: 'Approve',
-        produk: 'Produk 1',
-        brand: 'Brand 1',
-        type: 'Type 1',
-        rbp_gross: 'RBP Gross 1',
-        promo: 'Promo 1',
-        rbp_net: 'RBP Net 1',
-        rsp: 'RSP 1',
-        notes: 'Notes 1',
+        id: 1,
+        name: 'Januari',
+      },
+      {
+        id: 2,
+        name: 'Februari',
+      },
+      {
+        id: 3,
+        name: 'Maret',
+      },
+      {
+        id: 4,
+        name: 'April',
+      },
+      {
+        id: 5,
+        name: 'Mei',
+      },
+      {
+        id: 6,
+        name: 'Juni',
+      },
+      {
+        id: 7,
+        name: 'Juli',
+      },
+      {
+        id: 8,
+        name: 'Agustus',
+      },
+      {
+        id: 9,
+        name: 'September',
+      },
+      {
+        id: 10,
+        name: 'Oktober',
+      },
+      {
+        id: 11,
+        name: 'November',
+      },
+      {
+        id: 12,
+        name: 'Desember',
       },
     ],
+    dataWeekly: [
+      {
+        id: 1,
+        name: 'Week 1',
+      },
+      {
+        id: 2,
+        name: 'Week 2',
+      },
+      {
+        id: 3,
+        name: 'Week 3',
+      },
+      {
+        id: 4,
+        name: 'Week 4',
+      },
+    ],
+    formData: {
+      tahun: '',
+      bulan: '',
+      week: '',
+    },
+    pagination: {},
+    params: {
+      offset: 0,
+      limit: 2000,
+      tahun: '',
+      bulan: '',
+      week: '',
+    },
+    dataTable: [],
+    dataProduct: [],
+    brandList: [],
+    tipeList: [],
+    kemasanList: [],
+    dataDistrik: [],
     isLoading: false,
   },
 }
@@ -102,7 +172,54 @@ const mutations = {
 }
 
 const actions = {
-  async getAllTipe({ commit, state }, payload) {
+  async getDataTable({ commit, state }, payload) {
+    commit('changeWeeklyInput', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    let formData = {
+      offset: data.params.offset,
+      limit: data.params.limit,
+      tahun: data.params.tahun,
+      bulan: data.params.bulan,
+      week: data.params.week,
+    }
+
+    try {
+      const result = await apiClient.post(`/WPM/getData`, formData)
+
+      if (result.data.status == `false`) {
+        notification.error({
+          message: 'Error',
+          description: result.data.message,
+        })
+        await commit('changeWeeklyInput', {
+          isLoading: false,
+        })
+      } else {
+        await commit('changeWeeklyInput', {
+          dataTable: result.data.data,
+          isLoading: false,
+        })
+        notification.success({
+          message: 'Success',
+          description: result.data.message,
+        })
+      }
+    } catch (error) {
+      await commit('changeWeeklyInput', {
+        isLoading: false,
+      })
+      notification.error({
+        message: 'Error',
+        description: 'Maaf, terjadi kesalahan!',
+      })
+    }
+  },
+
+  async getMasterProduct({ commit, state }, payload) {
     commit('changeWeeklyInput', {
       isLoading: true,
     })
@@ -110,7 +227,42 @@ const actions = {
     const { data } = state
 
     try {
-      // const result = await apiClient.post(``)
+      const result = await apiClient.get(`/master/listSelectProduk`)
+
+      if (result.data.status == `false`) {
+        notification.error({
+          message: 'Error',
+          description: result.data.message,
+        })
+        await commit('changeWeeklyInput', {
+          isLoading: false,
+        })
+      } else {
+        await commit('changeWeeklyInput', {
+          dataProduct: result.data.data,
+          isLoading: false,
+        })
+      }
+    } catch (error) {
+      await commit('changeWeeklyInput', {
+        isLoading: false,
+      })
+      notification.error({
+        message: 'Error',
+        description: 'Maaf, terjadi kesalahan!',
+      })
+    }
+  },
+
+  async getAllBrand({ commit, state }, payload) {
+    commit('changeWeeklyInput', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    try {
+      const result = await apiClient.get(`/wpm/master-data/brand`)
 
       if (result.data.status == false) {
         await commit('changeWeeklyInput', {
@@ -122,6 +274,7 @@ const actions = {
         })
       } else {
         await commit('changeWeeklyInput', {
+          brandList: result.data.data,
           isLoading: false,
         })
       }
@@ -132,6 +285,110 @@ const actions = {
       notification.error({
         message: 'Error',
         description: 'Maaf, terjadi kesalahan!',
+      })
+    }
+  },
+
+  async getAllTipe({ commit, state }, payload) {
+    commit('changeWeeklyInput', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    try {
+      const result = await apiClient.get(`/wpm/master-data/tipe`)
+
+      if (result.data.status == false) {
+        await commit('changeWeeklyInput', {
+          isLoading: false,
+        })
+        notification.error({
+          message: 'Gagal',
+          description: result.data.message,
+        })
+      } else {
+        await commit('changeWeeklyInput', {
+          tipeList: result.data.data,
+          isLoading: false,
+        })
+      }
+    } catch (err) {
+      await commit('changeWeeklyInput', {
+        isLoading: false,
+      })
+      notification.error({
+        message: 'Error',
+        description: 'Maaf, terjadi kesalahan!',
+      })
+    }
+  },
+
+  async getAllKemasan({ commit, state }, payload) {
+    commit('changeWeeklyInput', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    try {
+      const result = await apiClient.get(`/wpm/master-data/kemasan`)
+
+      if (result.data.status == false) {
+        await commit('changeWeeklyInput', {
+          isLoading: false,
+        })
+        notification.error({
+          message: 'Gagal',
+          description: result.data.message,
+        })
+      } else {
+        await commit('changeWeeklyInput', {
+          kemasanList: result.data.data,
+          isLoading: false,
+        })
+      }
+    } catch (err) {
+      notification.error({
+        message: 'Error',
+        description: 'Maaf, terjadi kesalahan!',
+      })
+    }
+  },
+
+  async getDistrik({ commit, state }) {
+    commit('changeWeeklyInput', {
+      isLoading: true,
+    })
+
+    const { data } = state
+
+    let body = {
+      //   id_area:data.formData.selectedArea,
+      offset: data.params.offset,
+      limit: data.params.limit,
+    }
+    try {
+      const result = await apiClient.post('/filter/Distrik', body)
+
+      if (result.data.status == 'error') {
+        notification.error({
+          message: 'Error',
+          description: result.data.message,
+        })
+        await commit('changeWeeklyInput', {
+          isLoading: false,
+        })
+      } else {
+        await commit('changeWeeklyInput', {
+          dataDistrik: result.data.data,
+          isLoading: false,
+        })
+      }
+    } catch (error) {
+      notification.error({
+        message: 'Error',
+        description: 'Maaf, terjadi kesalahan',
       })
     }
   },
--- src/views/weeklyprice/weeklyInput/index.vue
+++ src/views/weeklyprice/weeklyInput/index.vue
@@ -3,70 +3,63 @@
     <a-row :gutter="[16, 16]" class="mb-3">
       <a-col :xs="24" :md="4">
         <a-select
+          v-model:value="weeklyInput.params.tahun"
           placeholder="Tahun"
           show-search
           class="w-100"
         >
           <a-select-option disabled value="">Pilih Tahun</a-select-option>
-          <a-select-option
-            data-toggle="tooltip"
-            data-placement="top"
-          >
-            Tahun
+          <a-select-option v-for="(tahun, index) in years" :value="tahun" :key="index">
+            {{ tahun }}
           </a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="4">
         <a-select
+          v-model:value="weeklyInput.params.bulan"
           placeholder="Bulan"
           show-search
           class="w-100"
         >
           <a-select-option disabled value="">Pilih Bulan</a-select-option>
           <a-select-option
-            data-toggle="tooltip"
-            data-placement="top"
+            v-for="(bulan, index) in weeklyInput.data_bulan"
+            :value="bulan.id"
+            :key="index"
           >
-            Bulan
+            {{ bulan.name }}
           </a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="4">
         <a-select
+          v-model:value="weeklyInput.params.week"
           placeholder="Week"
           show-search
           class="w-100"
+          @change="handleSubmit"
         >
           <a-select-option disabled value="">Pilih Week</a-select-option>
           <a-select-option
-            data-toggle="tooltip"
-            data-placement="top"
+            v-for="(week, index) in weeklyInput.dataWeekly"
+            :value="week.id"
+            :key="index"
           >
-            Week
+            {{ week.name }}
           </a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="12">
         <div class="d-flex justify-content-end">
-          <a-button
-            type="primary"
-            class="mr-2"
-            @click="showAddModal"
-          >
+          <a-button type="primary" class="mr-2" @click="showAddModal">
             <i class="fa fa-plus mr-2" />
             Tambah
           </a-button>
-          <a-button
-            type="primary"
-            class="mr-2"
-          >
+          <a-button type="primary" class="mr-2">
             <i class="fa fa-copy mr-2" />
             Duplicate Last Week
           </a-button>
-          <a-button
-            type="primary"
-            @click="showConfirmModal"
-          >
+          <a-button type="primary" @click="showConfirmModal">
             <i class="fa fa-upload mr-2" />
             Submit
           </a-button>
@@ -74,10 +67,53 @@
       </a-col>
     </a-row>
     <a-table
+      ref="table"
       :columns="weeklyInput.columns"
-      :data-source="weeklyInput.weeklyInputList"
+      :data-source="weeklyInput.dataTable"
+      :loading="weeklyInput.isLoading"
+      :pagination="weeklyInput.pagination"
+      :row-key="data => data"
       :scroll="{ x: 2000 }"
     >
+      <template #distrik="{ text }">
+        <span>{{ text.nm_wilayah }}</span>
+      </template>
+      <template #tahun="{ text }">
+        <span>{{ text.tahun }}</span>
+      </template>
+      <template #bulan="{ text }">
+        <span>{{ text.bulan }}</span>
+      </template>
+      <template #week="{ text }">
+        <span>{{ text.week }}</span>
+      </template>
+      <template #status="{ text }">
+        <span>{{ text.status }}</span>
+      </template>
+      <template #produk="{ text }">
+        <span>{{ text.nm_produk }}</span>
+      </template>
+      <template #brand="{ text }">
+        <span>{{ text.nm_brand }}</span>
+      </template>
+      <template #type="{ text }">
+        <span>{{ text.nm_type_produk }}</span>
+      </template>
+      <template #rbp_gross="{ text }">
+        <span>{{ text.rbp_gross }}</span>
+      </template>
+      <template #promo="{ text }">
+        <span>{{ text.promo }}</span>
+      </template>
+      <template #rbp_net="{ text }">
+        <span>{{ text.rbp_net }}</span>
+      </template>
+      <template #rsp="{ text }">
+        <span>{{ text.rsp }}</span>
+      </template>
+      <template #notes="{ text }">
+        <span>{{ text.notes }}</span>
+      </template>
       <template #action="{ text }">
         <div>
           <button
@@ -102,7 +138,7 @@
       </template>
     </a-table>
   </a-card>
-  
+
   <!-- Add Modal -->
   <a-modal
     v-model:visible="addModal"
@@ -115,85 +151,86 @@
   >
     <a-row :gutter="[24]">
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Distrik"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Distrik" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Distrik</a-select-option>
           <a-select-option value="distrik1">Distrik 1</a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Tahun"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Tahun" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Tahun</a-select-option>
-          <a-select-option value="2022">2022</a-select-option>
+          <a-select-option v-for="(tahun, index) in years" :value="tahun" :key="index">
+            {{ tahun }}
+          </a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Bulan"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Bulan" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Bulan</a-select-option>
-          <a-select-option value="1">Januari</a-select-option>
+          <a-select-option
+            v-for="(bulan, index) in weeklyInput.data_bulan"
+            :value="bulan.id"
+            :key="index"
+          >
+            {{ bulan.name }}
+          </a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Week"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Week" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Week</a-select-option>
-          <a-select-option value="week1">Week 1</a-select-option>
+          <a-select-option
+            v-for="(week, index) in weeklyInput.dataWeekly"
+            :value="week.id"
+            :key="index"
+          >
+            {{ week.name }}
+          </a-select-option>
         </a-select>
       </a-col>
     </a-row>
     <a-row :gutter="[24]">
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Produk"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Produk" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Produk</a-select-option>
           <a-select-option value="produk1">Produk 1</a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="brand"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="brand" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih brand</a-select-option>
-          <a-select-option value="brand1">Brand 1</a-select-option>
+          <a-select-option
+            v-for="(brand, index) in weeklyInput.brandList"
+            :value="brand.ID"
+            :key="index"
+          >
+            {{ brand.NAMA_BRAND }}
+          </a-select-option>
         </a-select>
       </a-col>
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Tipe"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Tipe" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Tipe</a-select-option>
-          <a-select-option value="tipe1">Tipe 1</a-select-option>
+          <a-select-option
+            v-for="(type, index) in weeklyInput.tipeList"
+            :value="type.ID"
+            :key="index"
+          >
+            {{ type.NAMA_TIPE_SEMEN }}
+          </a-select-option>
         </a-select>
       </a-col>
+
       <a-col :xs="24" :md="12" :lg="6">
-        <a-select
-          placeholder="Kemasan"
-          class="w-100 mb-4"
-          show-search
-        >
+        <a-select placeholder="Kemasan" class="w-100 mb-4" show-search>
           <a-select-option disabled value="">Pilih Kemasan</a-select-option>
-          <a-select-option value="kemasan1">Kemasan 1</a-select-option>
+          <a-select-option
+            v-for="(kemasan, index) in weeklyInput.kemasanList"
+            :value="kemasan.ID"
+            :key="index"
+          >
+            {{ kemasan.NAMA_KEMASAN }}
+          </a-select-option>
         </a-select>
       </a-col>
     </a-row>
@@ -215,7 +252,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 
 export default {
   data() {
@@ -227,8 +264,27 @@ export default {
     ...mapState({
       weeklyInput: state => state.weeklyInput.data,
     }),
+    years() {
+      const year = new Date().getFullYear()
+      return Array.from({ length: year - 2019 }, (value, index) => 2020 + index)
+    },
+  },
+  async mounted() {
+    await this.getMasterProduct()
+    await this.getAllBrand()
+    await this.getAllTipe()
+    await this.getAllKemasan()
   },
   methods: {
+    ...mapActions('weeklyInput', [
+      'getAllKemasan',
+      'getAllTipe',
+      'getAllBrand',
+      'getDataTable',
+      'getMasterProduct',
+      'getDistrik',
+    ]),
+
     showAddModal() {
       this.addModal = true
     },
@@ -247,6 +303,24 @@ export default {
     },
     saveWeeklyPrice() {
       this.addModal = false
+    },
+    async handleSubmit() {
+      if (
+        this.weeklyInput.params.tahun != '' &&
+        this.weeklyInput.params.bulan != '' &&
+        this.weeklyInput.params.week != ''
+      ) {
+        await this.getDataTable()
+        notification.success({
+          message: 'Success',
+          description: 'Data berhasil ditampilkan.',
+        })
+      } else {
+        notification.error({
+          message: 'Error',
+          description: 'Mohon Maaf. Data tahun, bulan dan week harap diisi.',
+        })
+      }
     },
   },
 }
