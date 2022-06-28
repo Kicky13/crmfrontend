69b3ae62cfa95fa11a80a61ad2bf98174c22b6e4
iqbalaulia <iqbal.developer16@gmail.com>
2022-06-28 22:30:58 +0700

    [update] fixing bug detail  supervisory visit role tso

--- src/views/reportingTSO/SupervisoryVisit/DetailSupervisory/index.vue
+++ src/views/reportingTSO/SupervisoryVisit/DetailSupervisory/index.vue
@@ -3,11 +3,11 @@
     <div class="row mb-2">
       <div class="col-md-4 col-xs-4">
         <a
-          @click="$router.push(`/reporting/supervisory-visit`)"
+          @click="$router.push(`/reporting/supervisory-visit-tso`)"
           class="font-weight-bold text-primary"
         >
           <i class="fa fa-chevron-left" aria-hidden="true"></i>
-          Kembali ke Reporting Supervisory Visit
+          Kembali ke Reporting Supervisory Visit TSO
         </a>
       </div>
     </div>
@@ -25,17 +25,34 @@
             />
           </div>
           <div class="supervisory_number_position">
-            <h5>MAJ.8676334.S03</h5>
+            <h5>
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.global_info &&
+                  reportingSupervisory.detailVisit.global_info[0] &&
+                  reportingSupervisory.detailVisit.global_info[0].id_reference_distributor
+              }}
+            </h5>
           </div>
           <div class="supervisory_company">
-            <span>MAJU ANUGRAH,PT</span>
+            <span>{{
+              reportingSupervisory.detailVisit &&
+                reportingSupervisory.detailVisit.global_info &&
+                reportingSupervisory.detailVisit.global_info[0] &&
+                reportingSupervisory.detailVisit.global_info[0].nm_distributor
+            }}</span>
           </div>
           <div class="supervisory_position">
             <span>Supervisor / AM :</span>
           </div>
           <div class="supervisory_name">
             <span>
-              ACIP LESMANA
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.global_info &&
+                  reportingSupervisory.detailVisit.global_info[0] &&
+                  reportingSupervisory.detailVisit.global_info[0].supervisor_am
+              }}
             </span>
           </div>
           <hr />
@@ -43,19 +60,37 @@
             <div class="points">
               <div class="d-flex">
                 <div class="p-2 title">Poin Perolehan</div>
-                <div class="ml-auto p-2 point">28</div>
+                <div class="ml-auto p-2 point">
+                  {{
+                    reportingSupervisory.detailVisit &&
+                      reportingSupervisory.detailVisit.nilai_info &&
+                      reportingSupervisory.detailVisit.nilai_info.poin_get
+                  }}
+                </div>
               </div>
             </div>
             <div class="points">
               <div class="d-flex">
                 <div class="p-2 title">Poin Maksimal</div>
-                <div class="ml-auto p-2 point">33</div>
+                <div class="ml-auto p-2 point">
+                  {{
+                    reportingSupervisory.detailVisit &&
+                      reportingSupervisory.detailVisit.nilai_info &&
+                      reportingSupervisory.detailVisit.nilai_info.poin_max
+                  }}
+                </div>
               </div>
             </div>
             <div class="points_counts">
               <div class="d-flex">
                 <div class="p-2 title">Hasil Penilaian</div>
-                <div class="ml-auto p-2 point">84.85</div>
+                <div class="ml-auto p-2 point">
+                  {{
+                    reportingSupervisory.detailVisit &&
+                      reportingSupervisory.detailVisit.nilai_info &&
+                      reportingSupervisory.detailVisit.nilai_info.nilai
+                  }}
+                </div>
               </div>
             </div>
           </div>
@@ -63,10 +98,69 @@
 
         <div class="card card-top supervisory_survey p-3">
           <span class="font-weight-bold title">Foto Survey</span>
-          <div
+          <!-- <div
             class="photo_survey"
             :style="'background-image: url(' + require('@/assets/LandingPage/images/FooterBG.webp')"
-          ></div>
+          ></div> -->
+
+          <div
+            v-if="
+              reportingSupervisory.detailVisit &&
+                reportingSupervisory.detailVisit.foto_info &&
+                reportingSupervisory.detailVisit.foto_info.length > 0
+            "
+          >
+            <a-carousel arrows>
+              <template #prevArrow>
+                <div class="custom-slick-arrow" style="left: 10px; z-index: 1">
+                  <left-circle-outlined />
+                </div>
+              </template>
+              <template #nextArrow>
+                <div class="custom-slick-arrow" style="right: 10px">
+                  <right-circle-outlined />
+                </div>
+              </template>
+
+              <div
+                v-for="(item, index) in reportingSupervisory.detailVisit.foto_info"
+                :key="`index_${index}`"
+              >
+                <div
+                  :style="'background-image: url(' + item.path_foto_visit"
+                  style="border-radius: 4px; background-repeat: no-repeat; width: 100%;height: 300px;background-position: 50% 50%; background-size: cover;"
+                ></div>
+              </div>
+            </a-carousel>
+          </div>
+          <div v-else>
+            <a-carousel arrows>
+              <template #prevArrow>
+                <div class="custom-slick-arrow" style="left: 10px; z-index: 1">
+                  <left-circle-outlined />
+                </div>
+              </template>
+              <template #nextArrow>
+                <div class="custom-slick-arrow" style="right: 10px">
+                  <right-circle-outlined />
+                </div>
+              </template>
+              <div>
+                <img
+                  src="@/assets/images/noimage.svg"
+                  class="img-fluid w-100"
+                  style="height:280px;"
+                />
+              </div>
+              <div>
+                <img
+                  src="@/assets/images/noimage.svg"
+                  class="img-fluid w-100"
+                  style="height:280px;"
+                />
+              </div>
+            </a-carousel>
+          </div>
         </div>
       </div>
       <div class="col-md-6 col-xs-6 ">
@@ -75,91 +169,34 @@
           <table class="mt-4 table_questions table">
             <thead>
               <tr>
-                <th>No</th>
+                <th width="30">No</th>
                 <th>Pertanyaan</th>
-                <th>Jawaban</th>
-                <th>Poin</th>
+                <th width="140">Jawaban</th>
+                <th width="100">Poin</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody v-for="(quest, index) in questions" :key="index">
               <tr>
                 <td colspan="4" class="mt-4">
                   <div class="btn btn-tabs w-100">
-                    Tahapan 1 - Perencanaan dan Persiapan
+                    {{ quest[0].nm_jenis_penilaian }}
                   </div>
                 </td>
               </tr>
-              <tr>
-                <td>1</td>
-                <td>Membawa katalog promosi / e-katalog</td>
-                <td>[1] - Ya</td>
+              <tr v-for="(quest_detail, indexQuest) in quest" :key="indexQuest">
+                <td>{{ indexQuest + 1 }}</td>
+                <td>{{ quest_detail.nm_penilaian_sales }}</td>
+                <td>
+                  [{{ quest_detail.nm_optional_jawaban == 'Tidak' ? 2 : 1 }}] -
+                  {{ quest_detail.nm_optional_jawaban }}
+                </td>
                 <td>
                   <div class="progress">
                     <div
                       class="progress-bar progress-bar-striped bg-success"
                       role="progressbar"
                       style="width: 10%"
-                      aria-valuenow="100"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Membawa Brosur / e-brosur</td>
-                <td>[1] - Ya</td>
-                <td>
-                  <div class="progress">
-                    <div
-                      class="progress-bar progress-bar-striped bg-success"
-                      role="progressbar"
-                      style="width: 100%"
-                      aria-valuenow="100"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                </td>
-              </tr>
-
-              <!-- Tahapan next -->
-              <tr>
-                <td colspan="4" class="mt-4">
-                  <div class="btn btn-tabs w-100">
-                    Tahapan 2 - Membuka Kunjungan
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>Membawa katalog promosi / e-katalog</td>
-                <td>[1] - Ya</td>
-                <td>
-                  <div class="progress">
-                    <div
-                      class="progress-bar progress-bar-striped bg-success"
-                      role="progressbar"
-                      style="width: 10%"
-                      aria-valuenow="100"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Membawa Brosur / e-brosur</td>
-                <td>[1] - Ya</td>
-                <td>
-                  <div class="progress">
-                    <div
-                      class="progress-bar progress-bar-striped bg-success"
-                      role="progressbar"
-                      style="width: 100%"
-                      aria-valuenow="100"
+                      :aria-valuenow="quest_detail.point"
                       aria-valuemin="0"
                       aria-valuemax="100"
                     ></div>
@@ -177,8 +214,15 @@
               <span>Objektif Pelaksanaan Survei</span>
             </div>
             <div class="body_detail mt-2">
-              Strike Rate, Customer Active, NOO, Sell-out Toko, Menanggapi Komplain, Perilaku
-              Salesman (Kedisiplinan Pelaksanaan Salesmanship)
+              {{
+                reportingSupervisory.detailVisit &&
+                reportingSupervisory.detailVisit.additional_info &&
+                reportingSupervisory.detailVisit.additional_info.objektif == ''
+                  ? 'Tidak tersedia'
+                  : reportingSupervisory.detailVisit &&
+                    reportingSupervisory.detailVisit.additional_info &&
+                    reportingSupervisory.detailVisit.additional_info.objektif
+              }}
             </div>
             <hr />
           </div>
@@ -187,7 +231,11 @@
               <span>Tanggal Survei</span>
             </div>
             <div class="body_detail mt-2">
-              Selasa, 27 Juni 2022
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  formatTimestamp(reportingSupervisory.detailVisit.additional_info.tanggal_survei)
+              }}
             </div>
             <hr />
           </div>
@@ -196,7 +244,19 @@
               <span>Toko Customer</span>
             </div>
             <div class="body_detail mt-2">
-              MEKAR JAYA ABADI (Telp. 081216593321)
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.toko_customer &&
+                  reportingSupervisory.detailVisit.additional_info.toko_customer.nama_toko
+              }}
+              (Telp.
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.toko_customer &&
+                  reportingSupervisory.detailVisit.additional_info.toko_customer.telp_toko
+              }})
             </div>
             <hr />
           </div>
@@ -205,9 +265,40 @@
               <span>Lokasi</span>
             </div>
             <div class="body_detail mt-2">
-              TANAH BARU RT.01 RW.06 - BOJONG GEDE - KABUPATEN BOGOR - JAWA BARAT
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi.alamat
+              }}
+              ,
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi.kota
+              }}
+              ,
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi.provinsi
+              }}
               <br />
-              [AREA 35 - REGION 2]
+              [{{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi.area
+              }}
+              -
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi &&
+                  reportingSupervisory.detailVisit.additional_info.lokasi.region
+              }}]
             </div>
             <hr />
           </div>
@@ -216,10 +307,42 @@
               <span>Atasan Sales Distributor</span>
             </div>
             <div class="body_detail mt-2">
-              <span class="badge badge-success">AM -> ACIP LESMANA</span>
-              <span class="badge badge-danger">SM -> DENI HERMAWAN</span>
-              <span class="badge badge-warning">SSM -> WAHYUDI</span>
-              <span class="badge badge-info">GSM -> EKO HERMANTO</span>
+              <span class="badge badge-success"
+                >AM ->
+                {{
+                  reportingSupervisory.detailVisit &&
+                    reportingSupervisory.detailVisit.additional_info &&
+                    reportingSupervisory.detailVisit.additional_info.atasan &&
+                    reportingSupervisory.detailVisit.additional_info.atasan.am
+                }}</span
+              >
+              <span class="badge badge-danger"
+                >SM ->
+                {{
+                  reportingSupervisory.detailVisit &&
+                    reportingSupervisory.detailVisit.additional_info &&
+                    reportingSupervisory.detailVisit.additional_info.atasan &&
+                    reportingSupervisory.detailVisit.additional_info.atasan.sm
+                }}</span
+              >
+              <span class="badge badge-warning"
+                >SSM ->
+                {{
+                  reportingSupervisory.detailVisit &&
+                    reportingSupervisory.detailVisit.additional_info &&
+                    reportingSupervisory.detailVisit.additional_info.atasan &&
+                    reportingSupervisory.detailVisit.additional_info.atasan.ssm
+                }}</span
+              >
+              <span class="badge badge-info"
+                >GSM ->
+                {{
+                  reportingSupervisory.detailVisit &&
+                    reportingSupervisory.detailVisit.additional_info &&
+                    reportingSupervisory.detailVisit.additional_info.evaluasi &&
+                    reportingSupervisory.detailVisit.additional_info.atasan.gsm
+                }}</span
+              >
             </div>
             <hr />
           </div>
@@ -228,7 +351,11 @@
               <span>Evaluasi & Tindakan Perbaikan</span>
             </div>
             <div class="body_detail mt-2">
-              Sales sangat rajin dan tepat waktu
+              {{
+                reportingSupervisory.detailVisit &&
+                  reportingSupervisory.detailVisit.additional_info &&
+                  reportingSupervisory.detailVisit.additional_info.evaluasi
+              }}
             </div>
           </div>
         </div>
@@ -238,9 +365,16 @@
 </template>
 <script>
 import { mapActions, mapState } from 'vuex'
+import { _ } from 'vue-underscore'
+import moment from 'moment'
 
 export default {
   nama: 'reportingSupervisory',
+  data() {
+    return {
+      questions: [],
+    }
+  },
   computed: {
     ...mapState({
       reportingSupervisory: state => state.reportingSupervisory.data,
@@ -250,17 +384,86 @@ export default {
       return Array.from({ length: year - 2021 }, (value, index) => 2022 + index)
     },
   },
-  mounted() {
+  async mounted() {
+    await this.getDataDetail()
     this.collapseClose()
   },
   methods: {
-    ...mapActions('reportingSupervisory', ['']),
+    ...mapActions('reportingSupervisory', ['getDataDetailSupervisoryVisit']),
     collapseClose() {
       this.$store.getters.settings.isMenuCollapsed = true
+    },
+    async getDataDetail() {
+      await this.getDataDetailSupervisoryVisit({
+        id_visited: this.$route.params.id_supervisory,
+      })
+
+      let dataDetail = Object.values(
+        this.reportingSupervisory.detailVisit.detail_nilai_info.detail_penilain,
+      )
+
+      this.questions = dataDetail
+      console.log(`this`, dataDetail)
+    },
+    formatTimestamp(time) {
+      if (time) {
+        return moment(time).format('DD MMMM YYYY')
+      } else {
+        return null
+      }
     },
   },
 }
 </script>
 <style lang="scss" scoped>
 @import '@/assets/scss/Reporting/SupervisoryVisit/index.scss';
+</style>
+
+<style scoped>
+/* For demo */
+/* .ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 300px;
+  line-height: 160px;
+  background: #fff;
+  overflow: hidden;
+}
+
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
+} */
+
+.ant-carousel :deep(.slick-slide) {
+  text-align: center;
+  height: 300px;
+  line-height: 160px;
+  background: #fff;
+  overflow: hidden;
+}
+
+.ant-carousel :deep(.slick-arrow.custom-slick-arrow) {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: black;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+  z-index: 1;
+}
+.ant-carousel :deep(.custom-slick-arrow:before) {
+  display: none;
+}
+.ant-carousel :deep(.custom-slick-arrow:hover) {
+  opacity: 0.5;
+}
+
+.ant-carousel :deep(.slick-slide h3) {
+  color: #fff;
+}
+
+#mapContainer {
+  width: 100%;
+  height: 60vh;
+  z-index: 0 !important;
+}
 </style>
