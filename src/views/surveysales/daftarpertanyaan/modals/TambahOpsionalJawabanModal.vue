945349d55c8184f7e37c6fe01a613007a4f3517b
varisatyananda <varisatyananda@gmail.com>
2022-06-29 13:28:34 +0700

    Update jenis isian

--- src/views/surveysales/daftarpertanyaan/modals/TambahOpsionalJawabanModal.vue
+++ src/views/surveysales/daftarpertanyaan/modals/TambahOpsionalJawabanModal.vue
@@ -29,8 +29,9 @@
       <a-form-item label="Jenis Isian">
         <a-select
           placeholder="Pilih jenis isian"
-          show-search
+          v-model:value="isNewIsian"
         >
+          <a-select-option disabled value="">Pilih jenis isian</a-select-option>
           <a-select-option value="0">Pilih salah satu</a-select-option>
           <a-select-option value="1">Pilih beberapa</a-select-option>
           <a-select-option value="2">Isian</a-select-option>
@@ -41,6 +42,8 @@
 </template>
 
 <script>
+import { notification } from 'ant-design-vue'
+
 export default {
   props: {
     modalVisible: Boolean,
@@ -50,6 +53,10 @@ export default {
       default: '',
     },
     newJawaban: {
+      type: String,
+      default: '',
+    },
+    newIsian: {
       type: String,
       default: '',
     },
@@ -66,6 +73,7 @@ export default {
     return {
       isNewJawaban: '',
       isNewPoin: '',
+      isNewIsian: '',
     }
   },
   watch: {
@@ -75,9 +83,39 @@ export default {
     newPoin() {
       this.isNewPoin = this.newPoin
     },
+    newIsian() {
+      this.isNewIsian = this.newIsian
+    },
   },
   methods: {
     handleOk() {
+      this.isNewJawaban = this.isNewJawaban.trim()
+      this.isNewPoin = this.isNewPoin.trim()
+
+      if (!this.isNewJawaban) {
+        notification.error({
+          message: 'Tambah Jawaban',
+          description: 'Kolom jawaban masih kosong',
+        })
+        return
+      }
+
+      if (!this.isNewPoin) {
+        notification.error({
+          message: 'Tambah Jawaban',
+          description: 'Kolom poin masih kosong',
+        })
+        return
+      }
+
+      if (!this.isNewIsian) {
+        notification.error({
+          message: 'Tambah Jawaban',
+          description: 'Kolom jenis isian masih kosong',
+        })
+        return
+      }
+
       const dataForm = {
         jawaban: this.isNewJawaban,
         poin: this.isNewPoin,
@@ -85,11 +123,13 @@ export default {
       this.$emit('handleOk', this.modalStatus, dataForm)
       this.isNewJawaban = ''
       this.isNewPoin = ''
+      this.isNewIsian = ''
     },
     handleCancel() {
       this.$emit('handleCancel')
       this.isNewJawaban = this.newJawaban
       this.isNewPoin = this.newPoin
+      this.isNewIsian = this.newIsian
     },
   },
 }
